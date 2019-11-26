0078870ba537a01c464a3e007f2aa8b42c3375a9
bjoluc <25661029+bjoluc@users.noreply.github.com>
2019-11-26 19:56:10 +0100

    setup rollup.js

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import pkg from "./package.json";
+import typescript from "rollup-plugin-typescript2";
+
+export default {
+  input: "src/index.tsx",
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+    },
+    {
+      file: pkg.module,
+      format: "es",
+    },
+  ],
+  external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
+  plugins: [
+    typescript({
+      typescript: require("typescript"),
+    }),
+  ],
+};
